93cb77f2111fc7f2721d82a2334a3fbeb6740a06
David Wesst <david@cocobokostudios.com>
2024-07-02 17:26:09 +0000

    added first validation tests for content

--- src/services/WDBlogPostContentService.cs
+++ src/services/WDBlogPostContentService.cs
@@ -1,25 +1,33 @@
 namespace DW.Website.Services;
 
 using DW.Website.Models;
+using ReverseMarkdown;
 
 public class WDBlogPostContentService: IBlogPostContentService
 {
-    public string GenerateBlogPostFileName(BlogPost post)
+    public string GenerateBlogPostFileName(BlogPost post) 
     {
         return $"{post.PublishDate.Year}-{post.PublishDate.Month}-{post.PublishDate.Day}-{post.Slug}";
     }
 
     public string GenerateBlogPostContent(BlogPost post, string author)
     {
-        var frontMatter = $@"---
-        title: {post.Title}
-        date: {post.PublishDate.ToString()}
-        originalurl: {post.OriginalURL}
-        authorId: {author}
-        ---";
-        var markdownContent = post.MDContent;
+        var frontMatter = $"---\n" +
+        $"title: {post.Title}\n" +
+        $"date: {post.PublishDate.ToString()}\n" +
+        $"originalurl: {post.OriginalURL}\n" +
+        $"authorId: {author}\n" +
+        "---";
+        var markdownContent = this.GenerateMDfromHTML(post.HTMLContent);
         var postContent = frontMatter + "\n\n" + markdownContent;
 
         return postContent;
+    }
+
+    private string GenerateMDfromHTML(string html)
+    {
+        var htmlConverter = new ReverseMarkdown.Converter();
+
+        return htmlConverter.Convert(html);
     }
 }
--- tests/WDBlogPostContentServiceScenarios.cs
+++ tests/WDBlogPostContentServiceScenarios.cs
@@ -1,22 +1,25 @@
 namespace DW.Website.Services.Tests;
 
+using System.ComponentModel;
+using System.Text.RegularExpressions;
 using DW.Website.Models;
 using DW.Website.Services;
 using Kekiri.Xunit;
 using Microsoft.Extensions.Logging;
 using Moq;
+using ReverseMarkdown.Converters;
 using Xunit;
 
 public class WDBlogPostContentServiceScenarios : Scenarios, IDisposable
 {
     BlogPost _blogPost;
     WDBlogPostContentService _service;
-    string _resultTitle;
+    string _result = String.Empty;
+    string _testAuthor = String.Empty;
 
     public WDBlogPostContentServiceScenarios()
     {
         _blogPost = new BlogPost();
-        _resultTitle = String.Empty;
         _service = new WDBlogPostContentService();
 
         CleanUp();
@@ -28,13 +31,52 @@ public class WDBlogPostContentServiceScenarios : Scenarios, IDisposable
     }
 
     [Scenario]
-    public void GeneratesWDFormattedTitle()
+    public void Generates_a_WD_Formatted_File_Name()
     {
         Given(a_service)
             .And(a_blog_post);
         When(generating_a_blog_post_title);
         Then(it_starts_with_the_date_and_ends_with_the_post_slug);
     }
+
+    [Scenario]
+    public void Generates_WD_Formatted_Content()
+    {
+        Given(a_service)
+            .And(a_blog_post)
+            .And(an_author_id);
+        When(generating_the_blog_post_contents);
+        Then(it_is_valid_markdown)
+            .And(always_has_frontmatter);
+    }
+
+    /**
+    [Scenario]
+    public void Includes_WD_FrontMatter_Values()
+    {
+        Given(a_service)
+            .And(a_blog_post)
+            .And(an_author_id);
+        When(generating_the_blog_post_contents);
+        Then(it_has_frontmatter_with_a_title)
+            .And(it_has_frontmatter_with_a_description)
+            .And(it_has_frontmatter_with_an_excerpt)
+            .And(it_has_frontmatter_with_the_author_id)
+            .And(it_has_frontmatter_with_the_publish_date)
+            .And(it_has_frontmatter_with_the_original_url)
+            .And(it_has_frontmatter_with_categories)
+            .And(it_has_frontmatter_with_tags);
+    }
+    [Scenario]
+    public void Updates_Media_Urls_to_WD_Relative_Locations()
+    {
+        Given(a_service)
+            .And(a_blog_post_with_media)
+            .And(an_author_id);
+        When(generating_the_blog_post_contents);
+        Then(all_media_urls_are_updated);
+    }
+    **/
 
     #region Setup & Teardown Methods
 
@@ -63,7 +105,12 @@ public class WDBlogPostContentServiceScenarios : Scenarios, IDisposable
             PublishDate = DateTime.Now,
             HTMLContent = "<h1>My test blog post</h1><p>Content goes here</p>"
         };
-    }
+   }
+
+   void an_author_id()
+   {
+        _testAuthor = "test_author_id";
+   }
 
     #endregion
 
@@ -71,7 +118,12 @@ public class WDBlogPostContentServiceScenarios : Scenarios, IDisposable
 
     void generating_a_blog_post_title()
     {
-        _resultTitle = _service.GenerateBlogPostFileName(_blogPost);
+        _result = _service.GenerateBlogPostFileName(_blogPost);
+    }
+
+    void generating_the_blog_post_contents()
+    {
+        _result = _service.GenerateBlogPostContent(_blogPost, _testAuthor);
     }
 
     #endregion
@@ -86,8 +138,27 @@ public class WDBlogPostContentServiceScenarios : Scenarios, IDisposable
         
         var expectedFileName = $"{expectedYear}-{expectedMonth}-{expectedDay}-{_blogPost.Slug}";
 
-        Assert.Equal(expectedFileName, _resultTitle);
+        Assert.Equal(expectedFileName, _result);
     }
 
+    void it_has_frontmatter_with_a_title()
+    {
+
+    }
+
+    void it_is_valid_markdown()
+    {
+        Assert.NotNull(_blogPost.MDContent);
+        Assert.Contains("# My test blog post\n\nContent goes here", _result);
+    }
+
+    void always_has_frontmatter()
+    {
+        var yamlPattern = @"^---\s*\n.*?\n---\s*\n";
+        var yamlRegex = new Regex(yamlPattern, RegexOptions.Singleline);
+
+        Assert.Matches(yamlRegex, _result);
+    }
+    
     #endregion
 }
